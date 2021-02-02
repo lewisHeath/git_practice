15d8d56db3c0e49cf1de7e880a9fafbf8caa458f
Lewis Heath <email@lewisheath.co.uk>
2021-02-02 00:12:09 +0000

    added multiply function

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello World!");
+int multiply(int a, int b){
+    return a * b;
+}
 
+int main() {
+    printf("Hello World!\n");
+    printf("4 x 5 = %d\n", multiply(4, 5));
     return 0;
 }
